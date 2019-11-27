a9330763c1bc7b8e8d1f6ab2b4a43d43065af1de
azhan-latif <44626911+azhan-latif@users.noreply.github.com>
2019-11-26 22:28:16 -0500

    Update blockchain.py
    Type hinting mistake

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -174,8 +174,8 @@ class Blockchain:
       if not transaction_bucket:
         break
 
-      chosen_transaction: Transaction = self.choose_random_transaction(transaction_bucket)
-      sender_account: Account, recipient_account: Account = self.get_transaction_accounts(chosen_transaction)
+      chosen_transaction = self.choose_random_transaction(transaction_bucket)
+      sender_account, recipient_account = self.get_transaction_accounts(chosen_transaction)
       if sender_account.is_contract():
         # Retrieve the function name from the data
         func_name = chosen_transaction.get_data()['func_name']
