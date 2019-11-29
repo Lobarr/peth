8310c8352325624c29e868fe5bcd33c964f184b7
Lobarr <jesulobaegunjobi@hotmail.com>
2019-11-29 10:48:17 -0800

    fix: completed demo script

--- scripts/demo.py
+++ scripts/demo.py
@@ -2,6 +2,7 @@ import base64
 import json
 import os
 import requests
+import pprint
 
 def encode_contract(contract_path: str) -> str:
   with open(contract_path, 'rb') as file:
@@ -30,9 +31,24 @@ transaction_data = {
 
 transaction_hash = requests.get(f"{api}/create_transaction?amount={transaction_data['amount']}&data={transaction_data['data']}&gas_limit={transaction_data['gas_limit']}&sender={transaction_data['sender']}&recipient={transaction_data['recipient']}&private_key={transaction_data['private_key']}").json()['data']
 
-print('sender account\n', sender_account, '\n')
-print('sender wallet\n', sender_wallet, '\n')
-print('encoded contract code\n', encoded_contract_code, '\n')
-print('contract account\n', contract_account, '\n')
-print('contract wallet\n', contract_wallet, '\n')
-print('transaction hash\n', transaction_hash, '\n')
+mempool = requests.get(f"{api}/show_mempool").json()['data']
+
+block = requests.get(f"{api}/mine_block").json()['result']
+
+verify_chain = requests.get(f"{api}/check_blockchain").json()['result']
+
+chain = requests.get(f"{api}/get_blocks").json()['result']
+
+accounts = requests.get(f"{api}/get_accounts").json()['data']
+
+print('sender account\n', pprint.pformat(sender_account), '\n')
+print('sender wallet\n', pprint.pformat(sender_wallet), '\n')
+print('encoded contract code\n', pprint.pformat(encoded_contract_code), '\n')
+print('contract account\n', pprint.pformat(contract_account), '\n')
+print('contract wallet\n', pprint.pformat(contract_wallet), '\n')
+print('transaction hash\n', pprint.pformat(transaction_hash), '\n')
+print('mempool', pprint.pformat(mempool), '\n')
+print('mined block', pprint.pformat(block), '\n')
+print('veify blockchain', pprint.pformat(verify_chain), '\n')
+print('blockchain', pprint.pformat(chain), '\n')
+print('Updated accounts', pprint.pformat(accounts), '\n')
